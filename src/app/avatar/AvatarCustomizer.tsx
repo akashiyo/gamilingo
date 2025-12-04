3e687ca3b9be746fdc07e2b03dca6370cb987875
Lucie <monjotlucie@gmail.com>
2025-12-04 17:30:02 +0100

    Les design de la page avatar avec la séléction par encore fonctionnelle avec la base de donnée

---
+++ src/app/avatar/AvatarCustomizer.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { useState } from "react";
+import "./AvatarCustomizer.css";
+
+const categories = [
+    { key: "eyes", icon: "/images/avatar/menu/eyes.svg" },
+    { key: "mouth", icon: "/images/avatar/menu/mouth.svg" },
+    { key: "hair", icon: "/images/avatar/menu/hair.svg" },
+    { key: "top", icon: "/images/avatar/menu/top.svg" },
+    { key: "hat", icon: "/images/avatar/menu/hat.svg" },
+    { key: "ears", icon: "/images/avatar/menu/ears.svg" },
+];
+
+const options: Record<string, string[]> = {
+    eyes: ["eyes_1", "eyes_2", "eyes_3", "eyes_4", "eyes_5", "eyes_6", "eyes_7", "eyes_8", "eyes_9", "eyes_10"],
+    mouth: ["mouth_1", "mouth_2", "mouth_3", "mouth_4", "mouth_5"],
+    hair: ["hair_1", "hair_2"],
+    top: ["top_1", "top_2", "top_3", "top_4"],
+    hat: ["hat_1", "hat_2", "hat_3", "hat_4"],  // <-- none pour enlever le chapeau
+    ears: ["ears_1", "ears_2", "ears_3", "ears_4"],
+};
+
+export default function AvatarCustomizer() {
+    const [selectedCategory, setSelectedCategory] = useState("eyes");
+
+    const [avatar, setAvatar] = useState({
+        eyes: "eyes_1",
+        mouth: "mouth_1",
+        hair: "hair_1",
+        hat: "none",   // <-- pas de chapeau par défaut
+        top: "top_1",
+        ears: "ears_1",
+    });
+
+    return (
+        <div className="avatar-container">
+
+            {/* Aperçu */}
+            <div className="avatar-preview-bg">
+                <div className="avatar-preview">
+
+                    <img src={`/images/avatar/eyes/${avatar.eyes}.svg`} className="eyes"/>
+
+                    <img src={`/images/avatar/mouth/${avatar.mouth}.svg`} className="mouth"/>
+                    <img src={`/images/avatar/ears/${avatar.ears}.svg`} className="ears"/>
+                    <img src={`/images/avatar/hair/${avatar.hair}.svg`} className="hair"/>
+
+                    {/* On n'affiche le chapeau que si ≠ none */}
+                    {avatar.hat !== "none" && (
+                        <img src={`/images/avatar/hat/${avatar.hat}.svg`} className="hat"/>
+                    )}
+
+                    <img src={`/images/avatar/top/${avatar.top}.svg`}   className={`top top-${avatar.top}`}/>
+                </div>
+            </div>
+
+            {/* Catégories */}
+            <div className="category-bar">
+                {categories.map((cat) => (
+                    <button
+                        key={cat.key}
+                        className={selectedCategory === cat.key ? "category-btn active" : "category-btn"}
+                        onClick={() => setSelectedCategory(cat.key)}
+                    >
+                        <img src={cat.icon} className="category-icon" />
+                    </button>
+                ))}
+            </div>
+
+            {/* Options */}
+            <div className="options-grid">
+                {options[selectedCategory].map((opt) => (
+                    <button
+                        key={opt}
+                        className="option-btn"
+                        onClick={() => setAvatar((prev) => ({ ...prev, [selectedCategory]: opt }))}
+                    >
+                        <img
+                            src={`/images/avatar/${selectedCategory}/${opt}.svg`}
+                            className="option-img"
+                        />
+                    </button>
+                ))}
+            </div>
+
+        </div>
+    );
+}
---
+++ src/app/avatar/page.tsx
@@ -0,0 +1,9 @@
+import AvatarCustomizer from "@/app/avatar/AvatarCustomizer";
+
+export default function AvatarPage() {
+    return (
+        <div className="p-4">
+            <AvatarCustomizer />
+        </div>
+    );
+}
